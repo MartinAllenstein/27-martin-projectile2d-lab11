e3727a2dc07bddb94e92f31ba533167ef40faacb
Martin <martin.allenstein@gmail.com>
2025-04-27 17:16:16 +0700

    Add Velocity

--- Assets/Scripts/Projectile 2D.cs
+++ Assets/Scripts/Projectile 2D.cs
@@ -2,15 +2,49 @@ using UnityEngine;
 
 public class Projectile2D : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Transform shootPoint;
+    [SerializeField] private GameObject target;
+    [SerializeField] private Rigidbody2D bulletPrefab;
 
-    // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //shoot raycast
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(ray.origin, ray.direction * 5f, Color.red, 3f);
+            
+            //get click point
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);
+            
+            //if hit
+            if (hit.collider != null)
+            {
+                target.transform.position = new Vector2(hit.point.x, hit.point.y);
+                Debug.Log("Hit " + hit.collider.name);
+
+                Vector2 projectileVelocity = CalculateProjectileVelocity(shootPoint.position, hit.point, 1f);
+                
+                //shoot bullet
+                Rigidbody2D shootBullet = Instantiate(bulletPrefab, shootPoint.position, Quaternion.identity);
+                
+                //add velocity
+                shootBullet.linearVelocity = projectileVelocity;
+            }
+        }
+    }
+
+    Vector2 CalculateProjectileVelocity(Vector2 origin, Vector2 target, float time)
+    {
+        Vector2 direction = target - origin;
         
+        //find velocity of x,y
+        float velocityX = direction.x / time;
+        float velocityY = direction.y / time + 0.5f * Mathf.Abs(Physics.gravity.y) * time;
+        
+        //get projectile vector
+        Vector2 projectileVelocity = new Vector2(velocityX, velocityY);
+        
+        return projectileVelocity;
     }
 }
